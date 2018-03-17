206dba7c3f09284bfce01f8ffff230a7bffc193b
Ken <khnoh@brown.edu>
2018-03-16 20:42:00 -0400

    finished tests

--- README.md
+++ README.md
@@ -20,3 +20,16 @@ python3 query.py stopwords index title [-t] [-v]
 * title: location of title index
 * -t: optional title flag, will print titles instead of id's
 * -v: optional verbose flag, will print (title, score) pairs instead of id's
+
+### Running tets:
+To run tests for create.py:
+```
+pytest create_test.py
+```
+
+To run tests for query.py, first need to run create.py for the small dataset.
+```
+python create.py stopwords.dat small.xml small_index.json small_title.json
+pytest query_test.py
+```
+All the tests are for QueryClass.py where the majority of the program is handled. There are no actual tests for query.py because query.py only contains the REPL.
--- query_test.py
+++ query_test.py
@@ -1,6 +1,22 @@
 import QueryClass as QC
 import json
 
+file = open('small_index.json', 'r')
+inverted = json.load(file)
+file.close()
+
+file=open('small_title.json','r')
+titleDict = json.load(file)
+file.close()
+
+file=open("tf_index.json",'r')
+tf_index=json.load(file)
+file.close()
+
+file=open("idf_index.json",'r')
+idf_index=json.load(file)
+file.close()
+
 def test_filter_and_stem():
     #tests the parsing
     assert QC.filter_and_stem('the a','stopwords.dat')==[]
@@ -36,22 +52,6 @@ def test_phrase_words():
     assert QC.phrase_words([temp,dict1,dict2])==[2]
 
 def test_small_queries():
-    file = open('small_index.json', 'r')
-    inverted = json.load(file)
-    file.close()
-
-    file=open('small_title.json','r')
-    titleDict = json.load(file)
-    file.close()
-
-    file=open("tf_index.json",'r')
-    tf_index=json.load(file)
-    file.close()
-
-    file=open("idf_index.json",'r')
-    idf_index=json.load(file)
-    file.close()
-
     query1 = QC.QueryFactory.create('space')
     results=query1.match(inverted,'stopwords.dat',titleDict,tf_index,idf_index)
     assert results==[(0, 1.0), (2, 1.0), (3, 1.0), (8, 1.0)]
@@ -92,8 +92,6 @@ def test_query_factory():
     assert isinstance(query4,QC.PhraseQuery)==True
     query5 = QC.QueryFactory.create('(space AND orange or)')
     assert isinstance(query5,QC.BooleanQuery)==True
-
-#print(QC.clean("nemo AND fish",'stopwords.dat')[0])
 
 def test_calc_doc_weight():
     file=open("tf_index.json",'r')
